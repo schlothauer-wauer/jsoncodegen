8810e2bdaeaa9c604d6c8468305b184d05ef583e
okieoth <eiko.thomas@gmx.net>
2017-05-30 11:39:31 +0200

    project init

---
+++ src/main/groovy/de/lisaplus/atlas/DoCodeGen.groovy
@@ -0,0 +1,13 @@
+package de.lisaplus.atlas
+
+import com.beust.jcommander.Parameter
+
+/**
+ * Created by eiko on 30.05.17.
+ */
+class DoCodeGen {
+    @Parameter(names = [ '-t', '--test' ], description = "Level of verbosity")
+    private String test
+
+
+}
