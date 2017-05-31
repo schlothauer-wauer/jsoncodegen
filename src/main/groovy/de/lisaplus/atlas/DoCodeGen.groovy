203ee0dfbb96e953bceac93f96afeaa6e59adc9b
okieoth <eiko.thomas@gmx.net>
2017-05-31 17:45:51 +0200

    start JSON parsing

--- src/main/groovy/de/lisaplus/atlas/DoCodeGen.groovy
+++ src/main/groovy/de/lisaplus/atlas/DoCodeGen.groovy
@@ -2,6 +2,8 @@ package de.lisaplus.atlas
 
 import com.beust.jcommander.JCommander
 import com.beust.jcommander.Parameter
+import de.lisaplus.atlas.model.Model
+import de.lisaplus.atlas.model.Type
 import groovy.json.JsonSlurper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -29,6 +31,12 @@ class DoCodeGen {
         log.info("model=${model}")
         log.info("outPutBase=${outputBaseDir}")
 
+        // remove in finished version - start
+        if (!model) {
+            model='src/test/resources/schemas/ProcessDataEvent.json'
+        }
+        // remove in finished version - end
+
         def modelFile = new File (model);
         if (!modelFile.isFile()) {
             log.error("path to model file doesn't point to a file: ${model}")
@@ -37,10 +45,76 @@ class DoCodeGen {
         else {
             log.info("use model file: ${model}")
         }
+        Model dataModel = buildModel(modelFile)
+        // TODO start CodeGen
+        dataModel.debugPrint()
+    }
 
+    private Model buildModel(File modelFile) {
         def jsonSlurper = new JsonSlurper()
         def objectModel = jsonSlurper.parse(modelFile)
-        print objectModel
+        if (!objectModel['$schema']) {
+            def errorMsg='model file seems to be no JSON schema'
+            log.error(errorMsg)
+            throw new Exception(errorMsg)
+        }
+        if (objectModel['properties']) {
+            // single type schema
+            return modelFromSingeTypeSchema(objectModel,modelFile.getName())
+        }
+        else if (objectModel['definitions']) {
+            // multi type schema
+            return modelFromMultiTypeSchema(objectModel)
+        }
+        else {
+            def errorMsg='unknown schema structure'
+            log.error(errorMsg)
+            throw new Exception(errorMsg)
+        }
+    }
+
+    private Model modelFromSingeTypeSchema(def objectModel, String modelFileName) {
+        Model model = initModel(objectModel)
+        def typeName = strFromMap(objectModel,'title')
+        if (!typeName) {
+            int lastDot = modelFileName.lastIndexOf('.')
+            if (lastDot==-1) {
+                typeName = modelFileName
+            }
+            else {
+                typeName = modelFileName.substring(0,lastDot)
+            }
+        }
+        typeName = string2Name(typeName)
+        Type newType = new Type()
+        newType.name = typeName
+        newType.description = strFromMap(objectModel,'description')
+        // TODO set properties
+        // TODO set required properties
+        model.types.add(newType)
+        return model
+    }
+
+    static String string2Name(String s) {
+        return s.replaceAll('[^a-zA-Z0-9]','_')
+    }
+
+    private Model modelFromMultiTypeSchema(def objectModel) {
+        Model model = initModel(objectModel)
+        return null
+    }
+
+    private String strFromMap(def map,String key,String defValue=null) {
+        def v = map[key]
+        if (!v) v = defValue
+        return v
+    }
+
+    private Model initModel(def objectModel) {
+        Model model = new Model()
+        model.title = strFromMap(objectModel,'title')
+        model.description = strFromMap(objectModel,'description')
+        return model
     }
 
     private static final Logger log=LoggerFactory.getLogger(DoCodeGen.class);
--- src/main/groovy/de/lisaplus/atlas/model/Model.groovy
+++ src/main/groovy/de/lisaplus/atlas/model/Model.groovy
@@ -1,21 +1,74 @@
 package de.lisaplus.atlas.model
 
+import org.apache.commons.lang3.builder.ToStringBuilder
+
 /**
  * Created by eiko on 31.05.17.
  */
 class Model {
     def title
+    def description
     /**
      * List of type definitions
      */
-    def types=[];
+    def types=[]
 
     /**
      * List of required types
      */
-    def requiredTypes=[];
+    def requiredTypes=[]
+
+    def debugPrint() {
+        println ToStringBuilder.reflectionToString(this);
+    }
 }
 
 class Type {
+    /**
+     * name for that type.
+     * Genarally build form JSON schema. For single type schemas first normalized content of title field is used. If
+     * ther is not title entry file name is used for type name.
+     * In multi type schemas key unter definitions section is used as type name
+     */
+    def name
+    /**
+     * List of properties, type of PropertyType
+     */
+    def properties=[]
+    def description
 
+    /**
+     *  List of required properties, String list with property names
+     */
+    def requiredProps=[];
+}
+
+enum PropertyType {
+    t_int,
+    t_string,
+    t_key,
+    t_boolean,
+    t_date,
+    t_date_time,
+    t_array
+}
+
+class Property {
+    def description
+    def name
+    PropertyType type
+    def format
+}
+
+class NumProperty extends Property {
+    def max
+    def exclusiveMax
+    def min
+    def exclusiveMin
+}
+
+class StringProperty extends Property {
+    def maxLength
+    def minLength
+    def pattern
 }
