95ef665c2ee985fdab02cb3618a408c35f3797d7
okieoth <eiko.thomas@gmx.net>
2017-07-03 15:38:33 +0200

    add additional produces entries for swagger

--- src/test/groovy/de/lisaplus/atlas/codegen/test/Swagger.groovy
+++ src/test/groovy/de/lisaplus/atlas/codegen/test/Swagger.groovy
@@ -1,8 +1,10 @@
 package de.lisaplus.atlas.codegen.test
 
+import junit.framework.Assert
 import org.junit.Test
 
 import static junit.framework.Assert.assertTrue
+import static junit.framework.Assert.assertEquals
 
 /**
  * Tests the swagger generator and the swagger template
@@ -36,4 +38,44 @@ class Swagger {
         assertTrue(new File(destFile).exists())
     }
 
+    @Test
+    void test_extraProduces_1() {
+        def destFile='tmp/user1.swagger'
+        de.lisaplus.atlas.DoCodeGen doCodeGen = new de.lisaplus.atlas.DoCodeGen()
+        doCodeGen.model='src/test/resources/test_schemas/ds/user.json'
+        doCodeGen.generators.add('swagger')
+        doCodeGen.outputBaseDir='tmp'
+        doCodeGen.generator_parameters.add('destFileName=user1.swagger')
+        doCodeGen.generator_parameters.add('removeEmptyLines=true')
+        doCodeGen.generator_parameters.add('host=api.lisaplus.de')
+        doCodeGen.generator_parameters.add('extraProduces=application/xml')
+        doCodeGen.run()
+        def createdFile = new File(destFile)
+        assertTrue(createdFile.exists())
+        def testResult = createdFile.text.indexOf('produces:\n  - application/json\n' +
+                '  - application/xml\n'+
+                'paths:')
+        assertTrue(testResult!=-1)
+    }
+
+    @Test
+    void test_extraProduces_2() {
+        def destFile='tmp/user2.swagger'
+        de.lisaplus.atlas.DoCodeGen doCodeGen = new de.lisaplus.atlas.DoCodeGen()
+        doCodeGen.model='src/test/resources/test_schemas/ds/user.json'
+        doCodeGen.generators.add('swagger')
+        doCodeGen.outputBaseDir='tmp'
+        doCodeGen.generator_parameters.add('destFileName=user2.swagger')
+        doCodeGen.generator_parameters.add('removeEmptyLines=true')
+        doCodeGen.generator_parameters.add('host=api.lisaplus.de')
+        doCodeGen.generator_parameters.add('extraProduces=application/xml, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        doCodeGen.run()
+        def createdFile = new File(destFile)
+        assertTrue(createdFile.exists())
+        def testResult = createdFile.text.indexOf('produces:\n  - application/json\n' +
+                '  - application/xml\n'+
+                '  - application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\n'+
+                'paths:')
+        assertTrue(testResult!=-1)
+    }
 }
