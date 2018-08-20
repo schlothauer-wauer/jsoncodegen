856ab828125c61c5762e4f076a807ae845ed30f2
Thomas, Eiko <eiko.thomas@lisaplus.de>
2018-08-20 08:22:28 +0200

    wip: XSDBuilder

--- src/main/groovy/de/lisaplus/atlas/builder/XSDBuilder.groovy
+++ src/main/groovy/de/lisaplus/atlas/builder/XSDBuilder.groovy
@@ -2,6 +2,8 @@ package de.lisaplus.atlas.builder
 
 import de.lisaplus.atlas.interf.IModelBuilder
 import de.lisaplus.atlas.model.Model
+import org.apache.xmlbeans.SchemaProperty
+import org.apache.xmlbeans.SchemaType
 import org.apache.xmlbeans.SchemaTypeSystem
 import org.apache.xmlbeans.XmlBeans
 import org.apache.xmlbeans.XmlObject
@@ -16,7 +18,26 @@ class XSDBuilder implements IModelBuilder {
         List<XmlObject> objectList = []
         objectList.add(object)
         SchemaTypeSystem sts = XmlBeans.compileXsd((XmlObject[])objectList.toArray(), XmlBeans.getBuiltinTypeSystem(), null);
-        println sts
+        def globalTypes = sts.globalTypes()
+        for (SchemaType type: globalTypes) {
+            // local name of the type
+            def localGlobalName =  type.getName().localPart
+            def baseType = type.getBaseType().getName().localPart
+            if (baseType) {
+                println "type: $localGlobalName extends $baseType"
+            }
+            else {
+                println "type: $localGlobalName"
+            }
+            def properties = type.getProperties()
+            for (SchemaProperty prop: properties) {
+                // how will we define type attributes as element or as XML attribute
+
+                def propName = prop.getName().localPart
+                def propType = prop.getType()
+                println "    $propName: $propType"
+            }
+        }
         Model model = new Model()
         model.title = 'dummy'
         model.description = 'not implemented, yet'
