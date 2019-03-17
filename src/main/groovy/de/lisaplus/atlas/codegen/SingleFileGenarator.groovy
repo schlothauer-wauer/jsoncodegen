569e0ddee9b297f86ceab8347df5aeece2db856d
Eiko Thomas <eiko.thomas@lisaplus.de>
2019-03-17 09:45:46 +0100

    import black- and whitelist handling for single file generators

--- src/main/groovy/de/lisaplus/atlas/codegen/SingleFileGenarator.groovy
+++ src/main/groovy/de/lisaplus/atlas/codegen/SingleFileGenarator.groovy
@@ -2,6 +2,7 @@ package de.lisaplus.atlas.codegen
 
 import de.lisaplus.atlas.interf.ICodeGen
 import de.lisaplus.atlas.model.Model
+import de.lisaplus.atlas.model.Type
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -31,7 +32,7 @@ abstract class SingleFileGenarator extends GeneratorBase implements ICodeGen {
         else {
             data.extraParam = [:]
         }
-
+        removeUnneededTypes(data,extraParams)
         def ergebnis = template.make(data)
 
         def destFileName = getDestFileName(model,extraParams)
@@ -43,5 +44,61 @@ abstract class SingleFileGenarator extends GeneratorBase implements ICodeGen {
                 ergebnis.toString()
         File file=new File("${destDir}/${destFileName}")
         file.write( resultString )
+    }
+
+    private void removeUnneededTypes (Map data, Map<String,String> extraParams) {
+        def blackListed=data.extraParam['blackListed']
+        def whiteListed=data.extraParam['whiteListed']
+
+        def neededAttrib = extraParams['containsAttrib']
+        def missingAttrib = extraParams['missingAttrib']
+        def neededTag = extraParams['neededTag']
+        def neededTagList = splitValueToArray(neededTag)
+
+        def ignoredTag = extraParams['ignoreTag']
+        def ignoredTagList = splitValueToArray(ignoredTag)
+        List<Type> neededTypes = []
+        data.model.types*.each { type ->
+            boolean handleNeeded = neededAttrib ? type.properties.find { prop ->
+                return prop.name==neededAttrib
+            } != null : true
+            boolean handleMissing = missingAttrib ? type.properties.find { prop ->
+                return prop.name==missingAttrib
+            } == null : true
+
+            boolean handleType=true;
+            if (whiteListed && (!whiteListed.contains(type.name))) {
+                handleType = false
+                println "ingnored by white-list: ${type.name}"
+            }
+            else if (blackListed && blackListed.contains(type.name)) {
+                handleType = false
+                println "ingnored by black-list: ${type.name}"
+            }
+
+            boolean handleTag = ignoredTagList ? type.tags.find { tag ->
+                return ignoredTagList.contains(tag)
+            } == null : true
+
+            if (handleTag && neededTagList) {
+                boolean allTagsFound=true;
+                neededTagList.each { needed ->
+                    if (!type.tags.contains(needed)) {
+                        allTagsFound = false
+                    }
+                }
+                handleTag = allTagsFound
+            }
+
+            if (handleType && handleNeeded && handleMissing && handleTag) {
+                neededTypes.add(type)
+            }
+        }
+        data.model.types = neededTypes
+    }
+
+    private List<String> splitValueToArray(String value) {
+        if (!value) return []
+        return value.indexOf(',')!=-1 ? value.split(',') : value.split(':')
     }
 }
