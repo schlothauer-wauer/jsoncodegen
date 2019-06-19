4b0c31dbe307fe1b4fabf5944f5484242e637e02
Eiko Thomas <eiko.thomas@lisaplus.de>
2019-06-19 09:16:57 +0200

    add automated docker image tags from version

--- docker/bin/image_conf.sh
+++ docker/bin/image_conf.sh
@@ -1,6 +1,10 @@
 imageBase=schlothauer/jsoncodegen
 imageTag=0.11.3
 
+if [ -f build.gradle ]; then
+    imageTag=`cat build.gradle | grep project.version | awk '{ print $3 }' | sed -e "s-'--g"`
+fi
+
 imageName="$imageBase:$imageTag"
 
 echo "$imageName"
