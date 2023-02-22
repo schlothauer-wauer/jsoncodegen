0f6cb0a8358079970c094d45c36d16fbf16576f3
Stefan Gürtler <stefan.guertler@swarco.com>
2023-02-22 20:24:30 +0100

    Made looking up the version independent from the working directory
    Ensures that docker/bin/build.sh can be called from within folder docker

--- docker/bin/image_conf.sh
+++ docker/bin/image_conf.sh
@@ -1,8 +1,10 @@
+scriptPos=${0%/*}
+
 imageBase=schlothauer/jsoncodegen
 imageTag=0.11.3
 
-if [ -f build.gradle ]; then
-    imageTag=`cat build.gradle | grep project.version | awk '{ print $3 }' | sed -e "s-'--g"`
+if [ -f $scriptPos/../../build.gradle ]; then
+    imageTag=$(cat $scriptPos/../../build.gradle | grep project.version | awk '{ print $3 }' | sed -e "s-'--g")
 fi
 
 imageName="$imageBase:$imageTag"
